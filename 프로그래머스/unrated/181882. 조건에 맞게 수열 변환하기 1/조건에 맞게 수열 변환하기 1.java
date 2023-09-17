6775908f018e0ea8e2a7c0458372cb97abf116c2
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2023-09-18 05:00:49 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.50 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = arr;
+
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] >= 50) {
+                if (arr[i] % 2 == 0) {
+                    answer[i] = arr[i] /2;
+                }
+            } else if (arr[i] % 2 == 1) {
+                answer[i] = arr[i] * 2;
+            }
+        }
+
+        return answer;
+    }
+}
