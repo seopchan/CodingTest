e9fe67561be7eb27324141a3e5ce75cb61fa8ddd
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2023-10-03 23:46:06 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<string> solution(vector<string> players, vector<string> callings) {
+    unordered_map<string, int> pla;
+    for (int i = 0; i < players.size(); i++) {
+        pla[players[i]] = i;
+    }
+    
+    for (const string& call: callings) {
+        if (pla.find(call) != pla.end()) {
+            int n = pla[call];
+            if (n > 0) {
+                string tmp = players[n-1];
+                players[n-1] = players[n];
+                players[n] = tmp;
+                
+                pla[players[n]] = n;
+                pla[players[n-1]] = n-1;
+            }
+        }
+    }
+    
+    return players;
+}
