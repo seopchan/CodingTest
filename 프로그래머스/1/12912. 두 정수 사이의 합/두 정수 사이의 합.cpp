cde0b522da1704bbcfc0d68eef6c8fdaebf4c83e
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2023-10-11 23:03:24 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long sum =0;
+    if (a > b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    for (int i=a; i<=b; i++) {
+        sum += i;
+    }
+    
+    return sum;
+}
