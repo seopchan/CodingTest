b705de83e09b52f48baaa62d29291164c3d9b3d6
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2023-12-19 19:41:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,7 @@
 def solution(s):
     dict = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
     
-    text = ''
-    for c in s:
-        if c.isalpha():
-            text += c
-            
-            if dict.get(text):
-                print(text)
-                s = s.replace(text, dict[text])
-                text = ''
-                print(s)
-                
+    for d in dict:
+        s = s.replace(d, dict[d])
+        
     return int(s)
