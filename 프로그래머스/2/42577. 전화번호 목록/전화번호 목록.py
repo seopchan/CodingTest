761f8d2910450bb2b968efcb4f2c72ee8b532287
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2023-12-26 04:59:44 +0900

    [level 2] Title: 전화번호 목록, Time: 748.17 ms, Memory: 242 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,16 @@
 def solution(phone_book):
-    phone_book.sort()
-    
-    for i in range(len(phone_book) - 1):
-        if phone_book[i+1].startswith(phone_book[i]):
+    trie = {}
+    for phone_number in phone_book:
+        current_node = trie
+        for number in phone_number:
+            if number not in current_node:
+                current_node[number] = {}
+            current_node = current_node[number]
+            if "end" in current_node:
+                return False
+        current_node["end"] = True
+
+        # 다른 번호가 현재 번호의 접두사인지 확인
+        if len(current_node) > 1:
             return False
     return True
