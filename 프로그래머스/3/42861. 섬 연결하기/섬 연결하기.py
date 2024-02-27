e1c76f82cd04c7f4b464427a337a7367eefb906e
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-02-27 17:06:52 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -8,30 +8,23 @@ def find(parent, i): # 노드 i의 루트 노드 찾기
         return i
     return find(parent, parent[i])
 
-def union(parent, rank, x, y): # Union-Find 알고리즘으로 Cycle형성 확인, 두 트리를 하나로 합침
+def union(parent, rank, x, y): # 두 트리를 하나로 합침
     xroot = find(parent, x)
     yroot = find(parent, y)
     # 두 루트 노드의 랭크(높이)를 비교해 랭크가 낮은 트리를 랭크가 높은 트리 아래에 붙임
     if rank[xroot] < rank[yroot]:
-        print(1)
         parent[xroot] = yroot
     elif rank[xroot] > rank[yroot]:
-        print(2)
         parent[yroot] = xroot
     else: # 랭크가 같으면 하나를 다른 하나의 부모로 하고 해당 루트의 랭크 +1
         parent[yroot] = xroot
-        print(3)
         rank[xroot] += 1
-    print(parent, rank)
 
 def solution(n, costs):
     # 간선들의 가중치 오름차순 정렬
     costs.sort(key=lambda x: x[2])
     parent = [i for i in range(n)]
     rank = [0] * n
-    print(costs)
-    print(parent)
-    print(rank)
     
     result = 0
     # 정렬된 간선 리스트에서 순서대로 사이클을 형성하지 않는 간선을 선택
@@ -39,9 +32,9 @@ def solution(n, costs):
         x, y, weight = cost
         xroot = find(parent, x)
         yroot = find(parent, y)
-        print("node:", x, y, "root:",xroot, yroot)
-        if xroot != yroot: # 사이클을 형성하는 간선을 제외
+        if xroot != yroot: # Union-Find 알고리즘으로 Cycle형성 확인, 사이클을 형성하는 간선을 제외
             result += weight
             union(parent, rank, xroot, yroot)
     
     return result
+
