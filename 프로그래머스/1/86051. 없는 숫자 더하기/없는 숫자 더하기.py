1f621c046beede066f6288b7f50a8c29114f0d88
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-13 11:42:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,14 +1,27 @@
-def solution(numbers):
+def solution1(numbers):
     sum_val = (0 + 9) * 10 / 2
     for n in numbers:
         sum_val -= n
     
     return sum_val
 
-# def solution(numbers):
-#     ls = [i for i in range(10)]
+def solution2(numbers):
+    ls = [i for i in range(10)]
     
-#     for n in numbers:
-#         ls[n] = 0
+    for n in numbers:
+        ls[n] = 0
     
-#     return sum(ls)
+    return sum(ls)
+
+def solution(numbers):
+    ls = [0] * 10
+    
+    for n in numbers:
+        ls[n] = 1
+    
+    sum_val = 0
+    for i, val in enumerate(ls):
+        if val == 0:
+            sum_val += i
+            
+    return sum_val
