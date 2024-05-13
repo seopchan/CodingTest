d4bd6253342a5b838f19704cd37e8e301d370011
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-13 16:00:17 +0900

    [level 2] Title: 롤케이크 자르기, Time: 648.67 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,28 +1,23 @@
+from collections import defaultdict
+
 def solution(topping):
     answer = 0
-    left = set()
-    right = set(topping)
+    left = defaultdict(int)
+    right = defaultdict(int)
     
     # 첫 토핑 상태 초기화 0:n
-    right_count = {}
     for t in topping:
-        if t in right_count:
-            right_count[t] += 1
-        else:
-            right_count[t] = 1
+        right[t] += 1
     
-    # 토핑을 순회하며 개수 비교
+    # 토핑 순회하며 개수 비교
     for t in topping:
-        left.add(t)
-        
-        right_count[t] -= 1
-        if right_count[t] == 0:
-            right.remove(t)
+        left[t] += 1
+        if right[t] == 1:
+            del right[t]
+        else:
+            right[t] -= 1
         
         if len(left) == len(right):
             answer += 1
-        
-    return answer
-    
     
     return answer
