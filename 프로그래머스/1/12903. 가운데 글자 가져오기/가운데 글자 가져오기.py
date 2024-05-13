f8b5810fcac08030674e03bc486b0c9cd6e5dd9f
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-13 12:17:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,8 +1,4 @@
 def solution(s):
-    l = len(s)
-    val = l // 2
+    mid, is_odd = divmod(len(s), 2)
     
-    if l % 2 == 0:
-        return s[val - 1 : val + 1]
-    else:
-        return s[val]
+    return s[mid] if is_odd else s[mid-1 : mid+1]
