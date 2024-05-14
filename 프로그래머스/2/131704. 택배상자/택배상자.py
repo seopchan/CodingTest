634985c1dc6aec2912433f704c5dacfd5636d7aa
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-14 12:49:13 +0900

    [level 2] Title: 택배상자, Time: 379.48 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -3,22 +3,13 @@ def solution(order):
     size = len(order)
     
     stack = []
-    current = 1
     
+    current = 1
     while current <= size:
-        if current == order[order_idx]:
-            order_idx += 1
-            current += 1
-        else:
-            if stack and stack[-1] == order[order_idx]:
-                stack.pop()
-                order_idx += 1
-            else:
-                stack.append(current)
-                current += 1
-
-        # 스택에서 순서대로 빼기
-        while stack and stack[-1] == order[order_idx]:
+        stack.append(current)
+        current += 1
+        
+        while stack and stack[-1] == order[order_idx]: 
             stack.pop()
             order_idx += 1
             
