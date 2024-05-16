931650e0c13c8e1f9de387ecebd453f9d2300abc
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-16 10:55:16 +0900

    [level 2] Title: 광물 캐기, Time: 16.61 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,9 +1,7 @@
 import math
 
 def solution(picks, minerals):
-    DIA_PICK = 'dia'
-    IRON_PICK = 'iron'
-    STONE_PICK = 'stone'
+    DIA_PICK, IRON_PICK, STONE_PICK = 'dia', 'iron', 'stone'
     
     piro = {
         'diamond': {DIA_PICK: 1, IRON_PICK: 5, STONE_PICK: 25}, 
@@ -11,33 +9,41 @@ def solution(picks, minerals):
         'stone': {DIA_PICK: 1, IRON_PICK: 1, STONE_PICK: 1}
     }
     
+    dia, iron, stone = picks
     n = len(minerals)
     min_piro = math.inf
     
-    def dfs(dia, iron, stone, mine_idx, total_piro):
-        nonlocal min_piro
+    def calc_new_piro(pick_type, mine_idx, total_piro):
+        new_piro = total_piro
+        for i in range(mine_idx, min(mine_idx + 5, n)):
+            new_piro += piro[minerals[i]][pick_type]
+        return new_piro
+    
+    def dfs(mine_idx, total_piro):
+        nonlocal min_piro, dia, iron, stone
+        
         if mine_idx >= n or (dia == 0 and iron == 0 and stone == 0):
             min_piro = min(min_piro, total_piro)
             return
         
         if dia > 0:
-            new_piro = total_piro
-            for i in range(mine_idx, min(mine_idx + 5, n)):
-                new_piro += piro[minerals[i]][DIA_PICK]
-            dfs(dia - 1, iron, stone, mine_idx + 5, new_piro)
+            dia -= 1
+            new_piro = calc_new_piro(DIA_PICK, mine_idx, total_piro)
+            dfs(mine_idx + 5, new_piro)
+            dia += 1
         
         if iron > 0:
-            new_piro = total_piro
-            for i in range(mine_idx, min(mine_idx + 5, n)):
-                new_piro += piro[minerals[i]][IRON_PICK]
-            dfs(dia, iron - 1, stone, mine_idx + 5, new_piro)
+            iron -= 1
+            new_piro = calc_new_piro(IRON_PICK, mine_idx, total_piro)
+            dfs(mine_idx + 5, new_piro)
+            iron += 1
         
         if stone > 0:
-            new_piro = total_piro
-            for i in range(mine_idx, min(mine_idx + 5, n)):
-                new_piro += piro[minerals[i]][STONE_PICK]
-            dfs(dia, iron, stone - 1, mine_idx + 5, new_piro)
+            stone -= 1
+            new_piro = calc_new_piro(STONE_PICK, mine_idx, total_piro)
+            dfs(mine_idx + 5, new_piro)
+            stone += 1
     
-    dfs(picks[0], picks[1], picks[2], 0, 0)
+    dfs(0, 0)
     
     return min_piro
