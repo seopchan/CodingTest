9a1a9e7dce0689ca7afdbcb32519c99e0c062c05
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-16 13:04:19 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.96 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,31 @@
+def solution(k, ranges):
+    def calc_space(x, y):
+        small = min(x, y)
+        big = max(x, y)
+        return big - ((big - small) / 2)
+    
+    seq = [k]
+    n = 0
+    areas = []
+    while k != 1:
+        n += 1
+        if k % 2 == 0:
+            k >>= 1
+        else:
+            k = k * 3 + 1
+        areas.append(calc_space(seq[-1], k))
+        seq.append(k)
+        
+    print(areas)
+    answer = []    
+    for a, b in ranges:
+        if n + b < a:
+            answer.append(-1)
+            continue
+        
+        if a == 0 and b == 0:
+            answer.append(sum(areas))
+        else:
+            answer.append(sum(areas[a:n+b]))
+
+    return answer
