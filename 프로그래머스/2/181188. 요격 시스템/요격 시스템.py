01ca91c9bfce530b1abbce9dda6eac49d3640a7f
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-17 12:13:20 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,5 +1,12 @@
 def solution(targets):
-    print(targets)
-    
+    # 끝지점(e) 기준 오름차순 정렬
+    targets.sort(key = lambda x:x[1])
     answer = 0
+    point = -1
+    
+    for s, e in targets:
+        if s >= point:
+            answer += 1
+            point = e
+    
     return answer
