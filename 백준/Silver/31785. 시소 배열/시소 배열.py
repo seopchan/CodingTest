210625f0f7437116a583eb01ddfd272870873759
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-30 02:27:08 +0900

    [Silver I] Title: 시소 배열, Time: 512 ms, Memory: 59604 KB -BaekjoonHub

--- 백준/Silver/31785. 시소 배열/시소 배열.py
+++ 백준/Silver/31785. 시소 배열/시소 배열.py
@@ -1,26 +1,30 @@
 import sys
 
 A = []
-QLen = int(sys.stdin.readline())
-removed = []
 
-for _ in range(QLen):
+def f1(x):
+    A.append(x)
+
+def f2(mid):
+    global A
+
+    frontSum = sum(A[:mid])
+    backSum = sum(A[mid:])
+
+    if frontSum <= backSum:
+        A = A[mid:]
+        print(frontSum)
+    elif frontSum > backSum:
+        A = A[:mid]
+        print(backSum)
+
+N = int(sys.stdin.readline())
+for _ in range(N):
     data = sys.stdin.readline().split()
+
     if data[0] == '1':
-        A.append(int(data[1]))
+        f1(int(data[1]))
     else:
-        mid = len(A) // 2
+        f2(len(A) // 2)
 
-        sumLeft = sum(A[:mid])
-        sumRight = sum(A[mid:])
-
-        if sumLeft > sumRight:
-            removed.append(sumRight)
-            A = A[:mid]
-        else:
-            removed.append(sumLeft)
-            A = A[mid:]
-
-for val in removed:
-    print(val)
 print(' '.join(map(str, A)))
