9c2f2ce107cb636f32d4680049cf1ed61140421c
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-30 11:13:45 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,15 +1,38 @@
-# 문자열 S의 서로 다른 부분 문자열
-# 경우의 수
-
 from sys import stdin
 
-string = stdin.readline().rstrip()
+s = stdin.readline().rstrip()
 
-N = len(string)
+def getSuffixArray(s):
+    suffixes = sorted([(s[i:], i) for i in range(len(s))])
+    return [suffix[1] for suffix in suffixes]
 
-answer = set()
-for start in range(N):
-    for end in range(start + 1, N + 1):
-        answer.add(string[start:end])
+def getLcpArray(s, suffixArray):
+    n = len(s)
 
-print(len(answer))
+    rank = [0] * n
+    lcp = [0] * n
+
+    for i, suffix in enumerate(suffixArray):
+        rank[suffix] = i
+
+    h = 0
+    for i in range(n):
+        if rank[i] > 0:
+            j = suffixArray[rank[i] - 1]
+            while i + h < n and j + h < n and s[i + h] == s[j + h]:
+                h += 1
+            lcp[rank[i]] = h
+            if h > 0:
+                h -= 1
+
+    return lcp
+     
+suffixArray = getSuffixArray(s)
+lcp = getLcpArray(s, suffixArray)
+
+n = len(s)
+numSubstrings = 0
+for i in range(n):
+    numSubstrings += n - suffixArray[i] - lcp[i]
+
+print(numSubstrings)
