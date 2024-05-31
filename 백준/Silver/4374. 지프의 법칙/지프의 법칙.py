8d40f7214fb0b84570a190506ad677440aa23314
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-05-31 12:42:46 +0900

    [Silver II] Title: 지프의 법칙, Time: 148 ms, Memory: 34796 KB -BaekjoonHub

--- 백준/Silver/4374. 지프의 법칙/지프의 법칙.py
+++ 백준/Silver/4374. 지프의 법칙/지프의 법칙.py
@@ -1,12 +1,5 @@
 # 4374 / 지프의 법칙
 
-# 책에서 k번째로 많이 나온 단어의 출현 빈도
-# 1/k에 비례
-
-# 첫 번째 많이 나온 단어 1
-# 두 번째 많이 나온 단어 첫 단어의 1/2
-# 세 번째 많이 나온 단어 첫 단어의 1/3
-
 # 딕셔너리로 등장 빈도 확인
 # Set으로 n회 등장 단어 관리
 
@@ -14,31 +7,21 @@ import re
 from sys import stdin
 from collections import defaultdict
 
-def isEndOfLine(input_line):
-    return input_line.strip() == ''
-
-def isEndOfText(input_text):
-    return input_text.strip() == "EndOfText"
-
 while True:
     input_line = stdin.readline().strip()
-    if isEndOfLine(input_line): 
+    if not input_line: 
         break
 
-    try:
-        n = int(input_line)
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-        continue
+    n = int(input_line)
 
     dic = defaultdict(int)
     appearedNTimes = set()
 
     while True:
         text = stdin.readline().strip()
-        if isEndOfText(text): break
+        if text == "EndOfText": break
 
-        # words = list(map(lambda word: word.strip(',').strip('.').strip('-').lower(), text.split()))
+        # 단어 경계에서 소문자 단어 추출, 대소문자 구분 x
         words = re.findall(r'\b[a-z]+\b', text.lower())
 
         for word in words:
@@ -48,7 +31,6 @@ while True:
                     appearedNTimes.add(word)
                 elif dic[word] > n:
                     appearedNTimes.discard(word)
-    
     
     if appearedNTimes:
         appearedNTimesList = sorted(appearedNTimes)
