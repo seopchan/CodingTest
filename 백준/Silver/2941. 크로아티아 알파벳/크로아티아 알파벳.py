4f2bed6dca579d9f9953d91e39ab2f8a7001cf42
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-01 11:01:48 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,33 +1,17 @@
-# 2941 / 크로아티아 알파벳
+import sys
 
-# [c=, c-, dz=, d-, lj, nj, s=, z=]
-# 목록에 없는 알파벳은 한 글자씩 세기
-# 모든 경우의 수 작성
+sentence = sys.stdin.readline().strip()
+cnt, idx = 0, 0
+two = {'c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z='}  # Changed to a set
+three = 'dz='
 
-from sys import stdin
+while idx < len(sentence):
+    if sentence[idx:idx+3] == three:
+        idx += 3
+    elif sentence[idx:idx+2] in two:
+        idx += 2
+    else:
+        idx += 1
+    cnt += 1
 
-string = list(stdin.readline().strip())
-length = len(string)
-alpha = 0
-i = 0
-
-while i < length:
-    char = string[i]
-    alpha += 1
-    
-    if i + 1 < length:
-        if char == 'c' and (string[i + 1] == '=' or string[i + 1] == '-'):
-            i += 1
-        elif char == 'd':
-            if string[i + 1] == '-':
-                i += 1
-            elif i + 2 < length and string[i + 1] == 'z' and string[i + 2] == '=':
-                i += 2
-        elif (char == 'l' or char == 'n') and string[i + 1] == 'j':
-            i += 1
-        elif (char == 's' or char == 'z') and string[i + 1] == '=':
-            i += 1
-    
-    i += 1
-    
-print(alpha)
+print(cnt)
