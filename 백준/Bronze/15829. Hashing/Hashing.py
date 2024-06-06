084b23d41ae9abf0134d81da278c5daaac5daec9
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-06 10:30:33 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,32 @@
+# 15829 / Hashing
+
+# 해시 함수
+# 입력 : 영문 소문자
+# a:1 ~ z:26
+
+# 문자열 수열 -> 정수로 변환 -> % M
+
+# 해시 충돌
+# r: 31 M: 1234567891
+
+from sys import stdin
+r = 31
+M = 1234567891
+
+def fn(num, i):
+    return (num * (r ** i))
+
+def alphaToNum(c):
+    return ord(c) - 96
+
+def main(string):
+    sum = 0
+    for i, c in enumerate(string):
+        sum += fn(alphaToNum(c), i) % M
+    
+    print(sum)
+    
+
+L = int(stdin.readline())
+string = stdin.readline().strip()
+main(string)
