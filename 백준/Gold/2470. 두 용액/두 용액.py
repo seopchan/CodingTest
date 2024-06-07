f724a722e1b1285498382cd70cd870d0de49be16
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-07 15:32:22 +0900

    [Gold V] Title: 두 용액, Time: 88 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -16,22 +16,22 @@ def find_closest_to_zero(n, num):
     left = 0
     right = n - 1
     
-    closest_sum = float('inf')
+    minAbs = float('inf')
     pair = (num[left], num[right])
     
     while left < right:
-        current_sum = num[left] + num[right]
+        sum = num[left] + num[right]
         
-        # 현재의 합이 더 0에 가까우면 갱신
-        if abs(current_sum) < abs(closest_sum):
-            closest_sum = current_sum
+        # 절댓값이 더 작으면 갱신 (0에 가까움)
+        if abs(sum) < abs(minAbs):
+            minAbs = sum
             pair = (num[left], num[right])
         
         # 합이 0보다 크면 오른쪽 포인터를 왼쪽으로 이동
-        if current_sum > 0:
+        if sum > 0:
             right -= 1
         # 합이 0보다 작으면 왼쪽 포인터를 오른쪽으로 이동
-        elif current_sum < 0:
+        elif sum < 0:
             left += 1
         # 합이 정확히 0이면 탐색 종료
         else:
