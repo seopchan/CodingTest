db963135a3138819e1b4bbfd4e153fd4f285039c
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-07 10:29:31 +0900

    [Silver V] Title: 숫자 카드, Time: 528 ms, Memory: 125628 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,31 +1,21 @@
 # 10815 / 숫자 카드
 
+"""
+set
+"""
 import sys
 input = sys.stdin.readline
 
-def binarySearch(arr, target):
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = left + (right - left) // 2
-
-        if arr[mid] == target:
-            return '1'
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return '0'
-
 N = int(input())
-cards = list(map(int, input().strip().split()))
+cards = set(map(int, input().strip().split()))
 M = int(input())
-targets = map(int, input().strip().split())
+targets = list(map(int, input().strip().split()))
 
-cards.sort()
-answer = []
+results = []
 for target in targets:
-    answer.append(binarySearch(cards, target))
-print(' '.join(answer))
+    if target in cards:
+        results.append(1)
+    else:
+        results.append(0)
+
+print(' '.join(map(str, results)))
