5657e5dc1244fca87be60227492f6edfdfcf923d
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-07 10:32:44 +0900

    [Silver V] Title: 숫자 카드, Time: 460 ms, Memory: 109236 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,5 +1,4 @@
 # 10815 / 숫자 카드
-
 """
 set
 """
@@ -9,13 +8,10 @@ input = sys.stdin.readline
 N = int(input())
 cards = set(map(int, input().strip().split()))
 M = int(input())
-targets = list(map(int, input().strip().split()))
+targets = map(int, input().strip().split())
 
 results = []
 for target in targets:
-    if target in cards:
-        results.append(1)
-    else:
-        results.append(0)
+    results.append('1') if target in cards else results.append('0')
 
-print(' '.join(map(str, results)))
+print(' '.join(results))
