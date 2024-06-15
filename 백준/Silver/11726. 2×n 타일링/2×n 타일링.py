23259dbfed97521d4c984d8ea388d59bedba5607
Seopchan_Kim <59127311+seopchan@users.noreply.github.com>
2024-06-15 10:56:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -2,13 +2,15 @@
 
 """
 피보나치 수열과 같은 규칙을 가짐
+((N-1) + (N-2)) % X = ((N - 1) % X + (N - 2) % X) % X
+-> 매번 모듈러 연산을 통해 메모리 사용량 줄이기
 """
 
 def fibo(n):
     a, b = 1, 2
     for _ in range(3, n + 1):
-        a, b = b, a + b
-    return b % 10_007
+        a, b = b, (a + b) % 10_007
+    return b
 
 def main():
     n = int(input())
